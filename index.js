5b3e6e9483af906ed82403c7d751c591e35eb795
UltraFerous <jandoga31@gmail.com>
2023-10-27 19:10:45 -0400

    Added feature isAllowed() and history

--- index.js
+++ index.js
@@ -1,22 +1,31 @@
 class Transaction {
   constructor(amount, account) {
-    this.amount  = amount;
+    this.amount = amount;
     this.account = account;
   }
   commit() {
-    this.account.balance += this.value;
+    if (this.isAllowed()) {
+      this.account.transactions.push(this.value);
+      this.account.balance += this.value;
+    }
+  }
+  isAllowed() {
+    if (this.account.balance + this.value < 0) {
+      return false;
+    }
+    return true;
   }
 }
 
 class Deposit extends Transaction {
   get value() {
-    return this.amount
+    return this.amount;
   }
 }
 
 class Withdrawal extends Transaction {
   get value() {
-    return -this.amount
+    return -this.amount;
   }
 }
 
@@ -25,6 +34,8 @@ class Account {
     this.username = username;
     // Have the account balance start at $0 since that makes more sense.
     this.balance = 0;
+    this.transactions = [];
+
   }
 }
 
@@ -41,5 +52,9 @@ console.log(t1.account.balance);
 const t2 = new Withdrawal(50.00, myAccount);
 t2.commit();
 console.log(t2.account.balance);
+
+const t3 = new Withdrawal(1000.00, myAccount);
+t3.commit();
+console.log(t3.account.balance);
 
 console.log('Ending Balance:', myAccount.balance);
